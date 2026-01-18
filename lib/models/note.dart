2847cfbc44243e930bf3c87e5f49baea2145610e
Ravishka Praveen <dmrpbandara@students.nsbm.ac.lk>
2026-01-18 19:35:00 +0530

    Added the model folder for the note class and generate note g file to Isar

---
+++ lib/models/note.dart
@@ -0,0 +1,12 @@
+import 'package:isar/isar.dart';
+
+//this line is needed to generate file
+//then run: run build_runner buildda
+
+part 'note.g.dart';
+
+@Collection()
+class Note {
+  Id id = Isar.autoIncrement;
+  late String text;
+}
